e65679e01846b389ae37f35fa493a391bb367228
JasonMac <jason.castro@boosmap.com>
2016-06-24 11:41:56 -0300

    Update utility trait

--- core/traits/utility.php
+++ core/traits/utility.php
@@ -6,7 +6,7 @@ trait utility{
 	public static function dump($var){
 		if($var){
 			echo "<pre>";
-		var_dump(str_replace("<", "|", str_replace(">", "|", escapeshellarg($var) )));
+		var_dump(str_replace("<", "|", str_replace(">", "|", $var)));
 		echo "</pre>";	
 		}else{
 			echo "NO VAR PASSED";
